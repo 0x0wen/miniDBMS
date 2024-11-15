5aa236a9e3044debdfac1aef43572713c97e56e8
Loxenary <110291644+Loxenary@users.noreply.github.com>
2024-11-15 10:21:39 +0700

    feat: documentation for Storage class

--- StorageManager/StorageManager.py
+++ StorageManager/StorageManager.py
@@ -5,23 +5,55 @@ from objects.Statistics import Statistics
 from objects.Rows import Rows  
 
 class StorageManager:
-
+    
     def __init__(self) -> None:
         pass
+
     
-    def readBlock(data_retrieval : DataRetrieval) -> Rows:
+    def readBlock(self, data_retrieval : DataRetrieval) -> Rows:
+        """
+        Returns Rows of data from harddisk
+        
+        Args:
+            data_retrieval : objects contains data to help determine which data to be retrieved from hard disk
+        
+        """
         pass
 
-    def writeBlock(data_retrieval: DataWrite) -> int:
+    def writeBlock(self ,data_write: DataWrite) -> int:
+        """
+        Returns the number of affected rows
+        
+        Args:
+            data_write : objects contains data to help determine which data to be retrieved from hard disk, contain modified data for modification operation, and new data for adddition operation
+        
+        """
         pass
 
-    def deleteBlock(data_deletion : DataDeletion) -> int:
+    def deleteBlock(self, data_deletion : DataDeletion) -> int:
+        """
+        Returns the number of removed rows
+
+        Args: 
+            data_deletion : objects contains data to help storage manager determince which data to be deleted.
+        """
         pass
 
-    def setIndex(table : str, column : str, index_type : str) -> None:
+    def setIndex(self, table : str, column : str, index_type : str) -> None:
+        """
+        Handle creation of index in a given table
+
+        Args : 
+            table : the table to be given index
+            column : certain column to be given index
+            index_type: type of index (B+ Tree or Hash)
+        """
         pass
 
     def getStats() -> Statistics:
+        """
+        Returns Statistics object that has number of tuples, number of blocks, size of tuple, blocking factor, and number of distinct values appear in r
+        """
         pass
 
     
