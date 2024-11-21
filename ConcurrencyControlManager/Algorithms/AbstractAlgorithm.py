f4a517ff57eacdc64e44027af7585ff33dda5062
M Fauzan Azhim <101320828+fauzanazz@users.noreply.github.com>
2024-11-21 12:28:00 +0700

    feat : Change Params Data

--- ConcurrencyControlManager/Algorithms/AbstractAlgorithm.py
+++ ConcurrencyControlManager/Algorithms/AbstractAlgorithm.py
@@ -1,15 +1,15 @@
 from abc import ABC, abstractmethod
-from Interface import Action
+from Interface import Action, Rows
 from Interface.Response import Response
 
 
 class AbstractAlgorithm(ABC):
     @abstractmethod
-    def run(self, db_object: int, transaction_id: int) -> None:
+    def run(self, db_object: Rows, transaction_id: int) -> None:
         raise NotImplementedError
 
     @abstractmethod
-    def validate(self, db_object: int, transaction_id: int, action: Action) -> Response:
+    def validate(self, db_object: Rows, transaction_id: int, action: Action) -> Response:
         raise NotImplementedError
 
     @abstractmethod
