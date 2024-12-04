fe70547827472e572b0313960ed85a819f83275d
Mohammad Akmal Ramadan <118426250+akmalrmn@users.noreply.github.com>
2024-12-04 13:47:48 +0700

    feat: add row field to execution result

--- Interface/ExecutionResult.py
+++ Interface/ExecutionResult.py
@@ -1,10 +1,13 @@
 from datetime import datetime
 from Interface.Rows import Rows  # Import Rows dari file rows.py
+from typing import List
 
 class ExecutionResult:
-    def __init__(self, transaction_id: int, timestamp: datetime, message: str, data: Rows, query: str):
+    def __init__(self, transaction_id: int, timestamp: datetime, message: str, data: Rows, query: str, rows: List[str]):
         self.transaction_id = transaction_id
         self.timestamp = timestamp
         self.message = message
         self.data = data
         self.query = query
+        self.rows = rows if rows is not None else []
+
